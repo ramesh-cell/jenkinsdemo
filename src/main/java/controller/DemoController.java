d1ea737f4886bdf6c011e8d0bc5d8685e3b4e33a
ramesh <rameshjava4plus@gmail.com>
2022-11-04 20:31:13 +0530

    323456:added business logic

--- src/main/java/controller/DemoController.java
+++ src/main/java/controller/DemoController.java
@@ -1,16 +1,22 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/call")
+@RequestMapping("api/v1/test")
 public class DemoController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String getHello(){
         return "Hello Jenkins";
+    }
+
+    @PostMapping("/say")
+    public String sayHiJenkins(){
+        return "sayHiJenkins";
     }
 
 }
